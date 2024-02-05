ce8b06ba8f52a30311b099a6babf3f2fa81857a3
bestPhumipat <ppbesto.9945@gmail.com>
2024-02-05 20:12:11 +0700

    fix cors

--- pet_service/internal/middlewares/cors.go
+++ pet_service/internal/middlewares/cors.go
@@ -1,18 +1,19 @@
 package middlewares
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"*"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"*"},
+		ExposeHeaders: []string{"Content-Length"},
+		// AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
 	})
 }
